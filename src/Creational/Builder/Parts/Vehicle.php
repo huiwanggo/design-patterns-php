a8dbe10a8354eaba527d0ebfbdd0697e20f77572
Huiwang <huiwanggo@gmail.com>
2021-09-15 22:58:15 +0800

    建造者模式

---
+++ src/Creational/Builder/Parts/Vehicle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DesignPatterns\Creational\Builder\Parts;
+
+abstract class Vehicle
+{
+    public function setPart(string $key, object $value)
+    {
+
+    }
+}
---
+++ tests/BuilderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use DesignPatterns\Creational\Builder\CarBuilder;
+use DesignPatterns\Creational\Builder\Director;
+use DesignPatterns\Creational\Builder\Parts\Car;
+use DesignPatterns\Creational\Builder\Parts\Truck;
+use DesignPatterns\Creational\Builder\TruckBuilder;
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+    public function testCanBuildTruck()
+    {
+        $truckBuilder = new TruckBuilder();
+        $newVehicle = (new Director())->build($truckBuilder);
+
+        $this->assertInstanceOf(Truck::class, $newVehicle);
+    }
+
+    public function testCanBuildCar()
+    {
+        $carBuilder = new CarBuilder();
+        $newVehicle = (new Director())->build($carBuilder);
+
+        $this->assertInstanceOf(Car::class, $newVehicle);
+    }
+}
